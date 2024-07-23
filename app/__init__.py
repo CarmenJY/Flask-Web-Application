059f9bdd50abd6e52884eb624eb919740302ab68
Jiaying Ji <ji000159@umn.edu>
2024-07-22 21:12:23 -0500

    users now are able to create accounts on this application; login, logout, and register features created

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,19 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__) #create a flask object
 app.config.from_object(Config)
-
 #Creates an instance of the SQLAlchemy class, passing the Flask app instance to it. 
 # This initializes the database connection and sets up the ORM based on the configuration settings loaded into the Flask app (e.g., SQLALCHEMY_DATABASE_URI).
 db = SQLAlchemy(app) # represent the database
-
 #Creates an instance of the Migrate class, passing the Flask app instance 
 #and the SQLAlchemy instance to it. This sets up Flask-Migrate, 
 # which provides database migration capabilities (e.g., creating and applying migrations).
 migrate = Migrate(app, db) # represent the data migration engine
+# Force users to login befoer they can view certain pages
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models # models will define the structure of the database
